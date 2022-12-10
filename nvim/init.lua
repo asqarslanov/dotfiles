f969be57c37234d6fa25335b73aed33c1290a7d4
Askar Arslanov <askarbink29@gmail.com>
2022-12-10 23:10:30 +0500

    Dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,27 @@
+require('keybindings')
+require('plugins')
+require('settings')
+
+-- if exists("g:neovide")
+	-- set guifont=FiraCode_Nerd_Font_Mono:h12
+	-- let g:neovide_cursor_vfx_mode = "ripple"
+-- endif
+-- 
+-- vnoremap <leader>y "+y
+-- nnoremap <leader>Y "+yg_
+-- nnoremap <leader>y "+y
+-- nnoremap <leader>yy "+yy
+-- 
+-- nnoremap <leader>p "+p
+-- nnoremap <leader>P "+P
+-- vnoremap <leader>p "+p
+-- vnoremap <leader>P "+P
+-- 
+-- set cursorcolumn
+-- set list
+-- " set listchars=tab:>-
+-- " set tabstop=4
+-- " set softtabstop=4
+-- " set shiftwidth=4
+-- " set smarttab
+-- " set expandtab
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,4 @@
+vim.opt.number = true;
+vim.opt.relativenumber = true;
+vim.opt.list = true;
+
