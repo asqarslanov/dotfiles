024d529d1238eb144b73c256e49e3b8bf25e55d8
Askar Arslanov <askarbink29@gmail.com>
2023-01-07 12:59:55 +0500

    Hyprland First Experience

---
+++ hypr/load_specific.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import socket
+
+hostname = socket.gethostname()
+username = os.getlogin()
+
+
+base_dir = f'/home/{username}/.config/hypr/'
+
+host_specific_dir = f'{base_dir}/host-specific'
+user_specific_dir = f'{base_dir}/user-specific'
+
+
+output = "# This file is written by load_specific.py. Do not edit.\n"
+
+if hostname + '.conf' in os.listdir(host_specific_dir):
+    output += "\n"
+    output += f"source = {host_specific_dir}/{hostname}.conf"
+    output += "\n"
+
+if username + '.conf' in os.listdir(user_specific_dir):
+    output += "\n"
+    output += f"source = {host_specific_dir}/{hostname}.conf"
+    output += "\n"
+
+
+with open(f'{base_dir}/loadspecific.conf', 'w') as output_file:
+    output_file.write(output)
