078be8a1af6a1bf30c3be1878e6a05f9b88016a3
Askar Arslanov <askarbink29@gmail.com>
2023-01-10 21:03:34 +0500

    Scripts

---
+++ scripts/sysinteraction.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+import subprocess
+import typing
+
+username = os.getlogin()
+variable_directory = f'/home/{username}/.local/vars'
+
+
+def execute(command: str) -> None:
+    arguments = command.split()
+    subprocess.run(arguments)
+
+
+def read_variable(name: str) -> typing.Any:
+    with open(f'{variable_directory}/{name}') as variable_file:
+        value = variable_file.read()
+
+    if value[-1] == '\n':
+        value = value[:-1]
+
+    match value:
+        case 'true':
+            return True
+        case 'false':
+            return False
+        case 'null':
+            return None
+
+    try:
+        return int(value)
+    except ValueError:
+        pass
+
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    return value
+
+
+def write_variable(name: str, value: typing.Any) -> None:
+    match value:
+        case True:
+            value = 'true'
+        case False:
+            value = 'false'
+        case None:
+            value = 'null'
+
+    with open(f'{variable_directory}/{name}', 'w') as variable_file:
+        print(value, file=variable_file)
+
+
+def update_directory(path: str) -> None:
+    if os.path.isfile(path):
+        os.remove(path)
+
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+
+    os.mkdir(path)
