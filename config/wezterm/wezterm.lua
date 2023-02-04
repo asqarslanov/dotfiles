d4477837e4bc0dd2933cd7f85fa335992889c6b7
Askar Arslanov <askarbink29@gmail.com>
2023-02-04 11:16:28 +0500

    Wal

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,46 @@
 local wezterm = require 'wezterm'
 
+
+function readall(filename)
+    local fh = assert(io.open(filename, 'rb'))
+    local contents = assert(fh:read('a'))
+    fh:close()
+    return contents
+end
+
+
+wal = wezterm.json_parse(
+    readall(
+        wezterm.home_dir .. '/.cache/wal/colors.json'
+    )
+)
+
+
 return {
-    color_scheme = 'Abernathy',
+    colors = {
+        ansi = {
+            wal.colors.color0,
+            wal.colors.color1,
+            wal.colors.color2,
+            wal.colors.color3,
+            wal.colors.color4,
+            wal.colors.color5,
+            wal.colors.color6,
+            wal.colors.color7,
+        },
+        background = wal.special.background,
+        brights = {
+            wal.colors.color8,
+            wal.colors.color9,
+            wal.colors.color10,
+            wal.colors.color11,
+            wal.colors.color12,
+            wal.colors.color13,
+            wal.colors.color14,
+            wal.colors.color15,
+        },
+        foreground = wal.special.foreground,
+    },
     default_cursor_style = 'SteadyBar',
     font = wezterm.font {
         family = 'FiraCodeNerdFontCompleteM-Retina',
