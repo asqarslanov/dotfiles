83225fea93bca1fcb79782dbdf8c3d1ff8af8a53
Askar Arslanov <askarbink29@gmail.com>
2023-03-01 09:05:27 +0500

    Yarn -> NPM

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -29,7 +29,7 @@ return {
     },
     {
         'barrett-ruth/live-server.nvim',
-        build = 'yarn global add live-server',
+        build = 'npm install -g live-server',
         config = true
     },
     {
