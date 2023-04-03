a896026d577637b137e3b8bf188c0f2c8f7c1971
Askar Arslanov <askarbink29@gmail.com>
2023-04-03 15:15:54 +0500

    Greetd Config

---
+++ config/greetd/envvars/desktop.sh
@@ -0,0 +1,7 @@
+export BROWSER=google-chrome-stable
+export EDITOR=nvim
+export FILEMANAGER=nemo
+export GTK_THEME=Dracula
+export PATH=$PATH:$HOME/.local/bin:$HOME/.local/binkctl
+export TERMINAL=kitty
+export XCURSOR_THEME=GoogleDot-Blue
