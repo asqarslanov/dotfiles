7abb05edd3ab6a7ca45a55054a1bdb375e7d9464
Askar Arslanov <askarbink29@gmail.com>
2023-04-05 19:54:59 +0500

    New Hyprland Launching

--- config/greetd/envvars/nvidia.sh
+++
@@ -1,6 +0,0 @@
-export GBM_BACKEND=nvidia-drm
-export LIBVA_DRIVER_NAME=nvidia
-export WLR_NO_HARDWARE_CURSORS=1
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export __GL_GSYNC_ALLOWED=1
-export __GL_VRR_ALLOWED=0
