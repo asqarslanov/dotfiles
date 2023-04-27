30efb905baae51d861dae5b5887bd6430f8fdff3
Askar Arslanov <askarbink29@gmail.com>
2023-04-28 00:07:22 +0500

    I like Nvchad so far

---
+++ config/nvchad/custom/init.lua
@@ -0,0 +1,13 @@
+vim.opt.confirm = true
+vim.opt.lazyredraw = true
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "→  ",
+    trail = "·"
+}
+vim.opt.wrap = false
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 6
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
--- config/nvchad/custom/plugins.lua
+++ config/nvchad/custom/plugins.lua
@@ -1,66 +1,66 @@
 local plugins = {
-  {
-    'williamboman/mason.nvim',
-    opts = {
-      ensure_installed = {
-        'bash-language-server',
-        -- 'clangd',
-        'html-lsp',
-        'json-lsp',
-        'lua-language-server',
-        'pyright',
-        'rust-analyzer'
-      }
+    {
+        'williamboman/mason.nvim',
+        opts = {
+            ensure_installed = {
+                'bash-language-server',
+                -- 'clangd',
+                'html-lsp',
+                'json-lsp',
+                'lua-language-server',
+                'pyright',
+                'rust-analyzer'
+            }
+        }
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            require 'plugins.configs.lspconfig'
+            require 'custom.configs.lspconfig'
+        end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        opts = {
+            ensure_installed = {
+                'bash',
+                -- 'c',
+                -- 'c_sharp',
+                -- 'cmake',
+                'comment',
+                'cpp',
+                'css',
+                'diff',
+                -- 'dockerfile',
+                'git_config',
+                'git_rebase',
+                'gitattributes',
+                'gitcommit',
+                'gitignore',
+                'html',
+                -- 'http',
+                'ini',
+                -- 'javascript',
+                -- 'jq',
+                'json',
+                'json5',
+                'jsonc',
+                -- 'latex',
+                'lua',
+                'luap',
+                -- 'make',
+                'markdown',
+                'markdown_inline',
+                'python',
+                'regex',
+                'rust',
+                'toml',
+                -- 'typescript',
+                'yaml'
+            }
+        }
     }
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      require 'plugins.configs.lspconfig'
-      require 'custom.configs.lspconfig'
-    end
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    opts = {
-      ensure_installed = {
-        'bash',
-        -- 'c',
-        -- 'c_sharp',
-        -- 'cmake',
-        'comment',
-        'cpp',
-        'css',
-        'diff',
-        -- 'dockerfile',
-        'git_config',
-        'git_rebase',
-        'gitattributes',
-        'gitcommit',
-        'gitignore',
-        'html',
-        -- 'http',
-        'ini',
-        -- 'javascript',
-        -- 'jq',
-        'json',
-        'json5',
-        'jsonc',
-        -- 'latex',
-        'lua',
-        'luap',
-        -- 'make',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'regex',
-        'rust',
-        'toml',
-        -- 'typescript',
-        'yaml'
-      }
-    }
-  }
 }
 
 return plugins
