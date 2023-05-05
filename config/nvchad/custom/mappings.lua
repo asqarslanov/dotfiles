25570eb931436015142c51c1a38aaebdbc791007
Askar Arslanov <askarbink29@gmail.com>
2023-05-05 15:54:27 +0500

    NvChad More Mappings

--- config/nvchad/custom/mappings.lua
+++ config/nvchad/custom/mappings.lua
@@ -1,24 +1,62 @@
 local M = {}
 
-M.disabled = {
+M.general = {
+    i = {
+        ['<C-Backspace>'] = {'<C-w>', 'erase word before'},
+        ['<C-a>'] = {'<ESC>I', 'beginning of line'}
+    },
     n = {
-        ['<leader>ph'] = '',
-        ['<leader>pt'] = ''
+        ['<leader>Q'] = {':quitall<CR>', 'quit'},
+        ['<leader>V'] = {'ggVG', 'select all lines'},
+        ['<leader>q'] = {':quit<CR>', 'quit'},
+        ['<leader>v'] = {'gg0vG$', 'select all'},
+        ['<leader>w'] = {':write<CR>', 'write'},
+    },
+    v = {
+        ['<leader>V'] = {'<ESC>ggVG', 'select all lines'},
+        ['<leader>v'] = {'<ESC>gg0vG$', 'select all'},
     }
 }
 
 M.clipboard = {
     n = {
-        ['<leader>y'] = {'"+y', 'yank...'},
-        ['<leader>Y'] = {'"+Y', 'yank line'},
+        ['<leader>D'] = {'"+D', 'delete rest'},
         ['<leader>P'] = {'"+P', 'paste before'},
-        ['<leader>p'] = {'"+p', 'paste after'}
+        ['<leader>X'] = {'"+X', 'delete before'},
+        ['<leader>Y'] = {'"+Y', 'yank line'},
+        ['<leader>d'] = {'"+d', 'delete...'},
+        ['<leader>p'] = {'"+p', 'paste after'},
+        ['<leader>x'] = {'"+x', 'delete char'},
+        ['<leader>y'] = {'"+y', 'yank...'}
     },
     v = {
-        ['<leader>y'] = {'"+y', 'yank...'},
+        ['<leader>C'] = {'"+C', 'change line'},
+        ['<leader>D'] = {'"+D', 'delete line'},
+        ['<leader>P'] = {'"+P', 'paste'},
+        ['<leader>S'] = {'"+S', 'substitute line'},
+        ['<leader>X'] = {'"+X', 'cut line'},
         ['<leader>Y'] = {'"+Y', 'yank line'},
-        ['<leader>P'] = {'"+P', 'paste before'},
-        ['<leader>p'] = {'"+p', 'paste after'}
+        ['<leader>c'] = {'"+c', 'change'},
+        ['<leader>d'] = {'"+d', 'delete'},
+        ['<leader>p'] = {'"+p', 'paste'},
+        ['<leader>s'] = {'"+s', 'substitute'},
+        ['<leader>x'] = {'"+x', 'cut'},
+        ['<leader>y'] = {'"+y', 'yank'}
+    }
+}
+
+M.disabled = {
+    n = {
+        ['<leader>D'] = '',
+        ['<leader>ph'] = '',
+        ['<leader>pt'] = '',
+        ['<leader>v'] = '',
+        ['<leader>wK'] = '',
+        ['<leader>wa'] = '',
+        ['<leader>wk'] = '',
+        ['<leader>wl'] = '',
+        ['<leader>wr'] = '',
+        ['<leader>x'] = ''
     }
 }
 
