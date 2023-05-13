82b096456ac9bfa791a7578614bc0556865b9345
Askar Arslanov <askarbink29@gmail.com>
2023-05-13 15:01:49 +0500

    Nvim Hop Fix

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -1,36 +1,23 @@
 return {
 	"phaazon/hop.nvim",
 
-	config = true,
-
-	keys = function()
+	config = function()
 		local hop = require("hop")
 
-		return {
-			{
-				"gf",
-				hop.hint_char1,
-			},
-			{
-				"gF",
-				hop.hint_words,
-			},
-			{
-				"gt",
-				function()
-					hop.hint_char1({ hint_offset = -1 })
-				end,
-			},
-			{
-				"gT",
-				function()
-					hop.hint_char1({ hint_offset = 1 })
-				end,
-			},
-			{
-				"g/",
-				hop.hint_patterns,
-			},
-		}
+		local set = vim.keymap.set
+
+		set("n", "gf", hop.hint_char1)
+		set("n", "gF", hop.hint_words)
+		set("n", "gt", function()
+			hop.hint_char1({ hint_offset = -1 })
+		end)
+		set("n", "gT", function()
+			hop.hint_char1({ hint_offset = 1 })
+		end)
+		set("n", "g/", hop.hint_patterns)
+
+		hop.setup()
 	end,
+
+	keys = { "gf", "gF", "gt", "gT", "g/" },
 }
