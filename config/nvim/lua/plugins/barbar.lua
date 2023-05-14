468663d70d64ace075a670626aa3fcf16181c36e
Askar Arslanov <askarbink29@gmail.com>
2023-05-14 14:16:26 +0500

    BarBar Looks Amazing

--- config/nvim/lua/plugins/barbar.lua
+++ config/nvim/lua/plugins/barbar.lua
@@ -9,36 +9,32 @@ return {
 	},
 
 	opts = {
-		auto_hide = true,
+		-- auto_hide = true,
 		icons = { button = false },
 	},
 
 	config = function(_, opts)
-		local set = vim.keymap.set
-
-		set("n", "<Tab>", "<Cmd>BufferNext<CR>")
-		set("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>")
-		set("n", "<Leader>q", "<Cmd>BufferClose<CR>")
-		set("n", "<Leader>Q", "<Cmd>BufferClose!<CR>")
-		set("n", "<Leader>t", "<Cmd>enew<CR>")
-		set("n", "<Leader>T", "<Cmd>BufferRestore<CR>")
-		set("n", "<M-1>", "<Cmd>BufferGoto 1<CR>")
-		set("n", "<M-2>", "<Cmd>BufferGoto 2<CR>")
-		set("n", "<M-3>", "<Cmd>BufferGoto 3<CR>")
-		set("n", "<M-4>", "<Cmd>BufferGoto 4<CR>")
-		set("n", "<M-5>", "<Cmd>BufferGoto 5<CR>")
-		set("n", "<M-6>", "<Cmd>BufferGoto 6<CR>")
-		set("n", "<M-7>", "<Cmd>BufferGoto 7<CR>")
-		set("n", "<M-8>", "<Cmd>BufferGoto 8<CR>")
-		set("n", "<M-9>", "<Cmd>BufferGoto 9<CR>")
-		set("n", "<M-0>", "<Cmd>BufferLast<CR>")
-		set("n", "<Leader><Tab>", "<Cmd>BufferMoveNext<CR>")
-		set("n", "<Leader><S-Tab>", "<Cmd>BufferMovePrevious<CR>")
-		set("n", "g<Tab>", "<Cmd>BufferPick<CR>")
-		set("n", "g<S-Tab>", "<Cmd>BufferPin<CR>")
+		vim.keymap.set("n", "<Tab>", "<Cmd>BufferNext<CR>")
+		vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>")
+		vim.keymap.set("n", "<Leader>q", "<Cmd>BufferClose<CR>")
+		vim.keymap.set("n", "<Leader>Q", "<Cmd>BufferClose!<CR>")
+		vim.keymap.set("n", "<Leader>t", "<Cmd>enew<CR>")
+		vim.keymap.set("n", "<Leader>T", "<Cmd>BufferRestore<CR>")
+		vim.keymap.set("n", "<M-1>", "<Cmd>BufferGoto 1<CR>")
+		vim.keymap.set("n", "<M-2>", "<Cmd>BufferGoto 2<CR>")
+		vim.keymap.set("n", "<M-3>", "<Cmd>BufferGoto 3<CR>")
+		vim.keymap.set("n", "<M-4>", "<Cmd>BufferGoto 4<CR>")
+		vim.keymap.set("n", "<M-5>", "<Cmd>BufferGoto 5<CR>")
+		vim.keymap.set("n", "<M-6>", "<Cmd>BufferGoto 6<CR>")
+		vim.keymap.set("n", "<M-7>", "<Cmd>BufferGoto 7<CR>")
+		vim.keymap.set("n", "<M-8>", "<Cmd>BufferGoto 8<CR>")
+		vim.keymap.set("n", "<M-9>", "<Cmd>BufferGoto 9<CR>")
+		vim.keymap.set("n", "<M-0>", "<Cmd>BufferLast<CR>")
+		vim.keymap.set("n", "<Leader><Tab>", "<Cmd>BufferMoveNext<CR>")
+		vim.keymap.set("n", "<Leader><S-Tab>", "<Cmd>BufferMovePrevious<CR>")
+		vim.keymap.set("n", "g<Tab>", "<Cmd>BufferPick<CR>")
+		vim.keymap.set("n", "g<S-Tab>", "<Cmd>BufferPin<CR>")
 
 		require("barbar").setup(opts)
 	end,
-
-	event = "VeryLazy",
 }
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -3,9 +3,14 @@ return {
 
 	opts = function()
 		local dracula = require("dracula")
+
 		return {
 			indent_color = dracula.colors().purple,
 			indent_color_zen = dracula.colors().comment,
+			tab_bar_bg_color = dracula.colors().bg,
+			tab_bar_active_color = dracula.colors().purple,
+			tab_bar_inactive_color = dracula.colors().comment,
+			tab_bar_separator_color = dracula.colors().selection,
 			ts_rainbow_colors = {
 				dracula.colors().pink,
 				dracula.colors().cyan,
@@ -24,13 +29,23 @@ return {
 		vim.g.colorscheme_plugin_name = plugin.url
 
 		-- plugins/mini-indentscope.lua
-		vim.cmd("highlight MiniIndentscopeSymbol guifg=" .. opts.indent_color .. " gui=nocombine")
+		vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = opts.indent_color })
 		vim.g.indent_color = opts.indent_color
 		vim.g.indent_color_zen = opts.indent_color_zen
 
+		-- plugins/barbar.lua
+		vim.api.nvim_set_hl(0, "BufferCurrent", { bg = opts.tab_bar_bg_color })
+		vim.api.nvim_set_hl(0, "BufferInactive", { bg = opts.tab_bar_bg_color, fg = opts.tab_bar_inactive_color })
+		vim.api.nvim_set_hl(0, "BufferInactiveMod", { bg = opts.tab_bar_bg_color, fg = opts.tab_bar_inactive_color })
+		vim.api.nvim_set_hl(0, "BufferInactiveSign", { bg = opts.tab_bar_bg_color, fg = opts.tab_bar_separator_color })
+		vim.api.nvim_set_hl(0, "BufferScrollArrow", { bg = opts.tab_bar_bg_color, fg = opts.tab_bar_inactive_color })
+		vim.api.nvim_set_hl(0, "BufferTabpageFill", { bg = opts.tab_bar_bg_color })
+		vim.api.nvim_set_hl(0, "BufferVisible", { fg = opts.tab_bar_inactive_color })
+		vim.api.nvim_set_hl(0, "BufferVisibleSign", { fg = opts.tab_bar_active_color })
+
 		-- plugins/treesitter.lua
 		for i, color in ipairs(opts.ts_rainbow_colors) do
-			vim.cmd("highlight TSRainbow" .. i .. " guifg=" .. color .. " gui=nocombine")
+			vim.api.nvim_set_hl(0, "TSRainbow" .. i, { fg = color })
 		end
 		vim.g.ts_rainbow_colors_count = #opts.ts_rainbow_colors
 	end,
