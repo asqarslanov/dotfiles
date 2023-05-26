80962767c38549c7afa1f8ade244b7d9179a43b4
Askar Arslanov <askarbink29@gmail.com>
2023-05-26 14:44:19 +0500

    Fish Some Settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,28 @@
+set -U fish_greeting
+
+# function fish_prompt
+#     echo
+#     echo \
+#         (set_color blue)(prompt_pwd) \
+#         (set_color normal)took (set_color yellow)(echo $CMD_DURATION)ms
+#     set_color green
+#     echo '$ '
+#     # printf '\n%s%s %s%s%s\n$ ' \
+#     #     (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+# end
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    alias 'cp'='cp --interactive --verbose'
+    alias 'ln'='ln --interactive --verbose'
+    alias 'mv'='mv --interactive --verbose'
+    alias 'rm'='echo Use either `trash` or `command rm` instead.; false'
+    alias 'remove'='command rm --interactive=once --verbose'
+    alias 'trash'='trash --verbose'
+
+    alias ':q!'='exit'
+    alias ':q'='exit'
+    alias 'info'='info --vi-keys'
+    alias 'ls'='exa --classify --icons --all --header'
+    alias 'neofetch'='echo neofetch: aliased to fastfetch; echo; fastfetch'
+    alias 'plz'='sudo'
 end
