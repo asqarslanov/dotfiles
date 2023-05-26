bb3e6e492bdd30800ee1e3363a9209f36af5c128
Askar Arslanov <askarbink29@gmail.com>
2023-05-26 20:49:36 +0500

    Fish Vi

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,6 +23,17 @@ if status is-interactive
         fastfetch
     end
 
+    function fish_user_key_bindings
+        fish_default_key_bindings -M insert
+        fish_vi_key_bindings --no-erase insert
+    end
+
+    set fish_cursor_default block blink
+    set fish_cursor_insert line blink
+    set fish_cursor_replace_one underscore blink
+    set fish_cursor_replace underscore blink
+    set fish_cursor_visual block
+
     # function fish_prompt
     #     echo
     #     echo \
