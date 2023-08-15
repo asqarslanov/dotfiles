fc41034bd2caabed8dbf7082288ea823ed939690
Askar Arslanov <askarbink29@gmail.com>
2023-08-15 10:18:30 +0300

    AstroNvim Surround

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -18,7 +18,7 @@ return {
 	-- },
 
 	-- Set colorscheme to use
-	-- colorscheme = "astrodark",
+	colorscheme = "dracula",
 
 	-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
 	-- diagnostics = {
@@ -28,31 +28,31 @@ return {
 
 	-- lsp = {
 	-- 	-- customize lsp formatting options
-	-- 	formatting = {
-	-- 		-- control auto formatting on save
-	-- 		format_on_save = {
-	-- 			enabled = true, -- enable or disable format on save globally
-	-- 			allow_filetypes = { -- enable format on save for specified filetypes only
-	-- 				-- "go",
-	-- 			},
-	-- 			ignore_filetypes = { -- disable format on save for specified filetypes
-	-- 				-- "python",
-	-- 			},
-	-- 		},
-	-- 		disabled = { -- disable formatting capabilities for the listed language servers
-	-- 			-- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-	-- 			-- "lua_ls",
-	-- 		},
-	-- 		timeout_ms = 1000, -- default format timeout
-	-- 		-- filter = function(client) -- fully override the default formatting function
-	-- 		--   return true
-	-- 		-- end
-	-- 	},
-	-- 	-- enable servers that you already have installed without mason
-	-- 	servers = {
-	-- 		-- "pyright"
-	-- 	},
-	-- },
+	formatting = {
+		-- 		-- control auto formatting on save
+		format_on_save = {
+			enabled = true, -- enable or disable format on save globally
+			-- 			allow_filetypes = { -- enable format on save for specified filetypes only
+			-- 				-- "go",
+			-- 			},
+			-- 			ignore_filetypes = { -- disable format on save for specified filetypes
+			-- 				-- "python",
+			-- 			},
+		},
+		-- 		disabled = { -- disable formatting capabilities for the listed language servers
+		-- 			-- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+		-- 			-- "lua_ls",
+		-- 		},
+		-- 		timeout_ms = 1000, -- default format timeout
+		-- 		-- filter = function(client) -- fully override the default formatting function
+		-- 		--   return true
+		-- 		-- end
+		-- 	},
+		-- 	-- enable servers that you already have installed without mason
+		-- 	servers = {
+		-- 		-- "pyright"
+		-- 	},
+	},
 
 	-- Configure require("lazy").setup() options
 	-- lazy = {
--- config/astronvim/plugins/community.lua
+++ config/astronvim/plugins/community.lua
@@ -1,9 +1,18 @@
 return {
-	-- Add the community repository of plugin specifications
-	-- "AstroNvim/astrocommunity",
-	-- example of importing a plugin, comment out to use it or add your own
-	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	"AstroNvim/astrocommunity",
 
-	-- { import = "astrocommunity.colorscheme.catppuccin" },
-	-- { import = "astrocommunity.completion.copilot-lua-cmp" },
+	{ import = "astrocommunity.colorscheme.dracula-nvim" },
+	{ import = "astrocommunity.editing-support.zen-mode-nvim" },
+	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.cpp" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.java" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.toml" },
+	{ import = "astrocommunity.pack.yaml" },
 }
