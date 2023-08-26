e861e6224620ff2346762b905c145521dfbcc5b1
Askar Arslanov <askarbink29@gmail.com>
2023-08-26 18:39:21 +0300

    AstroNvim Neovide JetBrains

--- config/astronvim/options.lua
+++ config/astronvim/options.lua
@@ -1,13 +1,13 @@
--- set vim options here (vim.<first_key>.<second_key> = value)
 return {
-	--   opt = {
-	--     -- set to true or false etc.
-	--     relativenumber = true, -- sets vim.opt.relativenumber
-	--     number = true, -- sets vim.opt.number
-	--     spell = false, -- sets vim.opt.spell
-	--     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-	--     wrap = false, -- sets vim.opt.wrap
-	--   },
+	opt = {
+		guifont = "JetBrainsMono Nerd Font:h13",
+		--     -- set to true or false etc.
+		--     relativenumber = true, -- sets vim.opt.relativenumber
+		--     number = true, -- sets vim.opt.number
+		--     spell = false, -- sets vim.opt.spell
+		--     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+		--     wrap = false, -- sets vim.opt.wrap
+	},
 	--   g = {
 	--     mapleader = " ", -- sets vim.g.mapleader
 	--     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
