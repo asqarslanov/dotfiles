81f92a8c0f01b7a7ef8485118520c8f8556fa01d
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2024-03-11 09:28:09 +0300

    fix(xkb): regular jcuken alongside regular qwerty, not wide

--- config/hypr/src/qwerty-colemak-dh-jcuken.sh
+++ config/hypr/src/qwerty-colemak-dh-jcuken.sh
@@ -3,7 +3,7 @@
 layout=$(hyprctl getoption input:kb_layout -j | jq .str)
 
 if [ "$layout" = '"colemak-dhw, jcuken-w"' ]; then
-  hyprctl keyword input:kb_layout "qwerty, jcuken-w"
+  hyprctl keyword input:kb_layout "qwerty, jcuken"
 else
   hyprctl keyword input:kb_layout "colemak-dhw, jcuken-w"
 fi
