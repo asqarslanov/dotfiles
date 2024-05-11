bb7315e3259f381615f83f9299c9d63d719f1f3f
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2024-05-11 20:48:56 +0300

    fix(hypr): 0.40, they changed their APIs...

--- config/eww/src/workspaces-listener.sh
+++ config/eww/src/workspaces-listener.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
 
-handle() {
-  case $1 in
-  workspace*) ~/.config/eww/bin/workspaces ;;
-  fullscreen*) ~/.config/eww/bin/workspaces ;;
-  openwindow*) ~/.config/eww/bin/workspaces ;;
-  closewindow*) ~/.config/eww/bin/workspaces ;;
-  movewindow*) ~/.config/eww/bin/workspaces ;;
-  esac
-}
-
-socat -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock |
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
   while read -r line; do
-    handle "$line"
+    ~/.config/eww/bin/workspaces
   done
