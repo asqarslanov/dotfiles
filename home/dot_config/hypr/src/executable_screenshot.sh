47e5a8fdc93cac28529ca834b9a1ef6edf7ded58
Asqar Arslanov <asqarslanov@gmail.com>
2025-11-07 19:41:37 +0300

    fix(hypr): improve screenshot logic

--- home/dot_config/hypr/src/executable_screenshot.sh
+++ home/dot_config/hypr/src/executable_screenshot.sh
@@ -2,27 +2,27 @@
 
 case "$1" in
 "interactive")
-  grim -o "$(hyprctl -j activeworkspace | jq --raw-output .monitor)" - |
+  grim -t ppm -o "$(hyprctl -j activeworkspace | jq --raw-output .monitor)" - |
     satty --filename=-
   ;;
 "file")
   mkdir --parents ~/Pictures/Screenshots/
   filename=~/Pictures/Screenshots/"$(date +%Y-%m-%d_%H-%M-%S)".png
   grim -o "$(hyprctl -j activeworkspace | jq --raw-output .monitor)" "$filename"
-  notify-send "File saved to '$filename'."
+  notify-send "Screenshot" "File saved to '$filename'."
   ;;
 "clipboard")
   filename=~/Pictures/Screenshots/%Y-%m-%d_%H-%M-%S.png
   grim -o "$(hyprctl -j activeworkspace | jq --raw-output .monitor)" - | wl-copy
-  notify-send "Copied to clipboard" Screenshot
+  notify-send "Screenshot" "Copied to clipboard."
   ;;
 "window")
-  grim -g "$(hyprctl -j clients | jq --argjson active "$(hyprctl monitors -j | jq -c '[.[].activeWorkspace.id]')" '.[] | select((.hidden | not) and (.workspace.id as $id | $active | contains([$id]))) | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' --raw-output | slurp -b "#000000b0" -c "#00000000")" - |
+  grim -g "$(hyprctl -j clients | jq --argjson active "$(hyprctl monitors -j | jq -c '[.[].activeWorkspace.id]')" '.[] | select((.hidden | not) and (.workspace.id as $id | $active | contains([$id]))) | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' --raw-output | slurp -b "#000000b0" -c "#00000000")" -t ppm - |
     satty --filename=-
   ;;
 "pixel")
   color=$(hyprpicker)
   wl-copy "$color"
-  notify-send "Copied to clipboard" "$color"
+  notify-send "Color pick" "Copied to clipboard: '$color'."
   ;;
 esac
